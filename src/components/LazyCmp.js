b1e643a602e54adb52cb579f2091edd17695c4b8
Parul_1994_2_5 <Parulsingh550.com>
2020-04-20 18:19:21 +0530

    Hoc

---
+++ src/components/LazyCmp.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+const LazyCmp=()=>{
+
+return(
+<div>
+<h1> Hi,i am lazy component. </h1>
+</div>
+)}
+export default LazyCmp; 
---
+++ src/components/Normal.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Normal=()=>{
+
+return(
+<div>
+<h1>Hi,i am Normal component.</h1>
+</div>
+)}
+export default Normal;
